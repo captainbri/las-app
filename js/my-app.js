e9b3e91e80aed978ffef3d49276f093a1e5d3421
Brian Santalone <santalone@wnet.org>
2014-07-26 15:19:01 -0400

    no message

--- js/my-app.js
+++ js/my-app.js
@@ -18,50 +18,24 @@ myApp.onPageInit('about', function (page) {
     });
 });
 
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back-blue"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
-
+//We can also add callback for all pages:
+myApp.onPageInit('*', function (page) {
+  ga_storage._trackPageview('/mobile-app/', page.name); 
+});
 
 
 
 
 // Pull to refresh content
-/*
+myApp.onPageInit('*', function (page) {
 var ptrContent = $$('.news-refresh');
     ptrContent.on('refresh', function(e) {
-        alert(1);
 		myApp.pullToRefreshDone(ptrContent);
 });
 
 var ptrContent2 = $$('.blog-refresh');
     ptrContent2.on('refresh', function(e) {
-        alert(2);
 		myApp.pullToRefreshDone(ptrContent2);
 });
-*/
+});
+// end refresh content
