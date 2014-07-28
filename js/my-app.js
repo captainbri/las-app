63a4c85b5cbb566bec2fd9a000ffde6ad308f2eb
Brian Santalone <santalone@wnet.org>
2014-07-28 14:55:18 -0400

    no message

--- js/my-app.js
+++ js/my-app.js
@@ -4,26 +4,21 @@ var myApp = new Framework7();
 // Export selectors engine
 var $$ = Framework7.$;
 
+
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
 
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
 
 //We can also add callback for all pages:
+/*
 myApp.onPageInit('*', function (page) {
   ga_storage._trackPageview('/mobile-app/', page.name); 
 });
 
-
+*/
 
 
 // Pull to refresh content
@@ -39,3 +34,30 @@ var ptrContent2 = $$('.blog-refresh');
 });
 });
 // end refresh content
+
+
+// infinite scroll posts.
+myApp.onPageInit('*', function (page) {
+var pagenum = 1;
+var loading = false;
+var num_messages = $$('.list-feed').data('max');
+var type = $$('.list-feed').data('type');
+// Attach 'infinite' event handler
+$$('.infinite-scroll').on('infinite', function () {
+
+  if (pagenum >= num_messages) {
+      myApp.detachInfiniteScroll($$('.infinite-scroll'));
+      $$('.infinite-scroll-preloader').remove();
+      return;
+    }
+else {
+	pagenum += 1;
+	loading = true;
+	$$.get('http://lifeasasister.org/ajax/load-more/?type='+type+'ajaxpage='+pagenum+'', function( data ) {
+		$$( ".list-feed ul" ).append( data );
+	});	
+}	
+ 
+});  
+}); 
+// END infinite scroll posts.
