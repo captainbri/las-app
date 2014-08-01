182ec63c06b3403f5fc076b378b6d5e1f9eaeac1
Brian Santalone <santalone@wnet.org>
2014-08-01 16:11:01 -0400

    no message

--- js/my-app.js
+++ js/my-app.js
@@ -47,18 +47,13 @@ var url = $$('a.button-comments').attr('href');
 	function iabLoadStop(event) {
         alert(event.type + ' - ' + event.url);
 		alert(url);
-			/*var p1 = "disqus.com/next/login-success";
+			var p1 = "disqus.com/next/login-success";
 			var p2 = "disqus.com/_ax/google/complete";
 			var p3 = "disqus.com/_ax/twitter/complete";
 			var p4 = "disqus.com/_ax/facebook/complete";
 			if (event.indexOf(p1) != -1 || event.indexOf(p2) != -1 || event.indexOf(p3) != -1 || event.indexOf(p4) != -1) { 		
-				alert(event); 
-				alert(url);
-				//window.location.href = url;
+				window.location.href = url;
 			}
-			*/
-		
-		
     }
 
 	
