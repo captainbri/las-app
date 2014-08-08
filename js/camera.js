46c25473faa41da2ccbecfdf459f959b660b0076
Brian Santalone <santalone@wnet.org>
2014-08-08 14:26:47 -0400

    no message

--- js/camera.js
+++ js/camera.js
@@ -1,44 +1,44 @@
 var pictureSource;   // picture source
 var destinationType; // sets the format of returned value
 
-    document.addEventListener("deviceready",onDeviceReady,false);
+document.addEventListener("deviceready",onDeviceReady,false);
+function onDeviceReady() {
+	pictureSource=navigator.camera.PictureSourceType;
+	destinationType=navigator.camera.DestinationType;
+}		
+    
+function onPhotoDataSuccess(imageData) {
+	var smallImage = document.getElementById('smallImage');
+	smallImage.style.display = 'block';
+	smallImage.src = "data:image/jpeg;base64," + imageData;
+}
 
-    function onDeviceReady() {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    }
+function onPhotoURISuccess(imageURI) {
+	var largeImage = document.getElementById('largeImage');
+	largeImage.style.display = 'block';
+	largeImage.src = imageURI;
+}
 
+function capturePhoto() {
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+	destinationType: destinationType.DATA_URL });
+}
 
-    function onPhotoDataSuccess(imageData) {
-      var smallImage = document.getElementById('smallImage');
-      smallImage.style.display = 'block';
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }
+function capturePhotoEdit() {
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+	destinationType: destinationType.DATA_URL });
+}
 
+function getPhoto(source) {
+	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+	//destinationType: destinationType.FILE_URI,
+	//sourceType: source 
+	destinationType: Camera.DestinationType.FILE_URI, 
+	sourceType:Camera.PictureSourceType.PHOTOLIBRARY
+	
+	});
+}
 
-    function onPhotoURISuccess(imageURI) {
-      var largeImage = document.getElementById('largeImage');
-      largeImage.style.display = 'block';
-      largeImage.src = imageURI;
-    }
-
-
-    function capturePhoto() {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL });
-    }
-
-    function capturePhotoEdit() {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-        destinationType: destinationType.DATA_URL });
-    }
-
-    function getPhoto(source) {
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    }
-
-    function onFail(message) {
-      alert('Failed because: ' + message);
-    }
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
