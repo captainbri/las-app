9717a58e085ae3e843adc9ae4bf468ec2ea00f6b
Brian Santalone <santalone@wnet.org>
2014-11-03 11:24:40 -0500

    no message

--- js/my-app.js
+++ js/my-app.js
@@ -210,3 +210,13 @@ myApp.onPageInit('*', function (page) {
 	  $$('.sys-terms').show();
 	});	
 });
+
+
+
+
+function fixiFrame () {
+   if ((android) && (androidVersion > 3)) {
+      iFrameContentHeight = document.getElementById('theiFrame').contentDocument.body.offsetHeight;
+      document.getElementById('theiFrame').style.height = iFrameContentHeight + 'px';
+   }
+}
