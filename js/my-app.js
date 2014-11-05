4fff2d27024d9df073c0e2cdc514f360e9692e31
Brian Santalone <santalone@wnet.org>
2014-11-05 17:59:45 -0500

    no message

--- js/my-app.js
+++ js/my-app.js
@@ -67,12 +67,15 @@ var url = $$('a.button-comments').attr('href');
 	
 	function iabLoadStop(event) {
 			var curr = event.url;
+			
+			if(event.url == "http://lifeasasister.org/ajax/disqus/?close"){iabRef.close();}
+			
 			var p1 = "disqus.com/next/login-success";
 			var p2 = "disqus.com/_ax/google/complete";
 			var p3 = "disqus.com/_ax/twitter/complete";
 			var p4 = "disqus.com/_ax/facebook/complete";
 			var p5 = "/ajax/disqus/close/";
-			if (curr.indexOf(p1) != -1 || curr.indexOf(p2) != -1 || curr.indexOf(p3) != -1 || curr.indexOf(p4) != -1 || curr.indexOf(p5) != -1) { 	
+			if (curr.indexOf(p1) != -1 || curr.indexOf(p2) != -1 || curr.indexOf(p3) != -1 || curr.indexOf(p4) != -1) { 	
 				window.open(url, '_blank', 'location=no,closebuttoncaption=Close Window');
 			}
     }
